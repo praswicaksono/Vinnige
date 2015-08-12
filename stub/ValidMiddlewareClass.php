982b03238bfb0c5e0f32e5ed8b58d9c47cf3775c
Prasetyo <jowy@oiry.net>
2015-08-12 18:18:18 +0700

    fix test failing on assert valid middleware

--- stub/ValidMiddlewareClass.php
+++ stub/ValidMiddlewareClass.php
@@ -15,9 +15,10 @@ class ValidMiddlewareClass implements MiddlewareInterface
     /**
      * @param ServerRequestInterface $request
      * @param ResponseInterface $response
+     * @param callable $next
      * @return ResponseInterface
      */
-    public function __invoke(ServerRequestInterface $request, ResponseInterface $response)
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next)
     {
         return $response;
     }
